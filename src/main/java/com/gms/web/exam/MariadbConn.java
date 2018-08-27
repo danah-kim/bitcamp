d064352071bc51e113372a32cfb53ac78a5c5b6d
danah <danah@192.168.0.2>
2018-08-27 12:51:00 +0900

    JUnit Test 완료

---
+++ src/main/java/com/gms/web/exam/MariadbConn.java
@@ -0,0 +1,31 @@
+package com.gms.web.exam;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MariadbConn {
+	public String exam() {
+		Connection conn;
+		Statement stmt;
+		String result = "";
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/mariadb", "mariadb", "mariadb");
+            stmt = conn.createStatement();
+ 
+            String sql = "SELECT * FROM MEMBER";
+            ResultSet rs = stmt.executeQuery(sql);
+            if(rs.next()) {
+                result = rs.getString("USERID");
+            }else {
+            	result = "연결실패";
+            }
+            System.out.println("결과 : "+result);
+         } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+	}
+}
