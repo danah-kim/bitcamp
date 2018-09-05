ea331bf2d9b828784219bbb883ebf277c77b6967
sweetmilkys <sweetmilky.dev@gmail.com>
2018-09-05 10:05:45 +0900

    calender 수정

--- MySchedule/app/src/main/java/app/rstone/com/myschedule/Main.java
+++ MySchedule/app/src/main/java/app/rstone/com/myschedule/Main.java
@@ -1,19 +1,16 @@
 package app.rstone.com.myschedule;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.CalendarView;
-import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -23,7 +20,8 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        final Context ctx = Main.this;
+        Context ctx = Main.this;
+        class Mydate{ String year, month, day; }
         TextView today = findViewById(R.id.today);
         CalendarView calender = findViewById(R.id.calender);
         TimePicker time = findViewById(R.id.time);
@@ -34,9 +32,10 @@ public class Main extends AppCompatActivity {
         final TextView minute = findViewById(R.id.minute);
         calender.setVisibility(View.VISIBLE);
         time.setVisibility(View.INVISIBLE);
-        calender.setMinDate(Long.parseLong(new SimpleDateFormat("yyyyMMdd", Locale.KOREA).format(new Date())));
+        today.setText(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA).format(new Date()));
         time.setHour(Integer.parseInt(new SimpleDateFormat("HH").format(new Date())));
         time.setMinute(Integer.parseInt(new SimpleDateFormat("mm").format(new Date())));
+        final Mydate m =new Mydate();
 
         findViewById(R.id.rdoCalendar).setOnClickListener((View v)->{
             calender.setVisibility(View.VISIBLE);
@@ -46,16 +45,28 @@ public class Main extends AppCompatActivity {
             calender.setVisibility(View.INVISIBLE);
             time.setVisibility(View.VISIBLE);
         });
+        calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                m.year = year + "";
+                m.month = month+1 + "";
+                m.day = dayOfMonth + "";
+            }
+        });
+
         findViewById(R.id.btnEnd).setOnClickListener((View v)->{
-            Calendar cal = Calendar.getInstance();
-            cal.setTimeInMillis(calender.getDate());
-            year.setText(cal.get(Calendar.YEAR)+"");
-            month.setText(cal.get(Calendar.MONTH)+"");
-            day.setText(cal.get(Calendar.DATE)+"");
+            year.setText(m.year);
+            month.setText(m.month);
+            day.setText(m.day);
             hour.setText(time.getHour()+"");
             minute.setText(time.getMinute()+"");
+            String result = year.getText().toString() + "-"
+                    + month.getText().toString() + "-"
+                    + day.getText().toString() + "-"
+                    + hour.getText().toString() + ":"
+                    + minute.getText().toString();
 
-            //Toast.makeText(ctx,"선택결과:"+result,Toast.LENGTH_LONG).show();
+            Toast.makeText(ctx,"선택결과:"+result,Toast.LENGTH_LONG).show();
         });
 
     }
